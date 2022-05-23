c7d5b5c63ae31ddc440c66ad2e49ea35194b348e
bcoalova <coalovaboris@gmail.com>
2022-05-23 18:47:51 -0300

    only counter

--- src/components/CommingSoon.jsx
+++ src/components/CommingSoon.jsx
@@ -1,10 +1,46 @@
 import { Typography } from '@mui/material'
+import { WEDDING_DATE } from '../constants'
+import useBackCount from '../hooks/useBackCount'
 import FullHeightCenter from './FullHeightCenter'
 
 function CommingSoon() {
+    const [intervaleD] = useBackCount(WEDDING_DATE)
+
     return (
-        <FullHeightCenter>
-            <Typography variant='h1'>Comming Soon...</Typography>
+        <FullHeightCenter sx={{ textAlign: 'center' }}>
+            {intervaleD && (
+                <>
+                    <Typography p={2} variant='h1'>
+                        {intervaleD.months > 0 && (
+                            <>
+                                En{' '}
+                                <Typography variant='h1' component='span'>
+                                    {intervaleD.months}
+                                </Typography>{' '}
+                                meses,
+                            </>
+                        )}
+                        {intervaleD.days > 0 && (
+                            <>
+                                <Typography variant='h1' component='span'>
+                                    {' '}
+                                    {intervaleD.days}
+                                </Typography>{' '}
+                                días y
+                            </>
+                        )}{' '}
+                        <Typography variant='h1' component='span'>
+                            {intervaleD.hours < 10 ? '0' + intervaleD.hours : intervaleD.hours}:
+                        </Typography>
+                        <Typography variant='h1' component='span'>
+                            {intervaleD.minutes < 10 ? '0' + intervaleD.minutes : intervaleD.minutes}:
+                        </Typography>
+                        <Typography variant='h1' component='span'>
+                            {intervaleD.seconds < 10 ? '0' + intervaleD.seconds : intervaleD.seconds}
+                        </Typography>
+                    </Typography>
+                </>
+            )}
         </FullHeightCenter>
     )
 }
