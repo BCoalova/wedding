decc69da8b1b1afe1648597d056a60deeec473ee
bcoalova <coalovaboris@gmail.com>
2022-05-23 19:51:16 -0300

    hidden all nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -30,94 +30,23 @@ function Nav() {
         <AppBar position='fixed' sx={{ width: '100%' }}>
             <Container maxWidth='xxl' sx={{ w: '100%' }}>
                 <Toolbar disableGutters>
-                    <Box component='img' src={maruYBoris} width={100} />
-                    {/* <Typography
-                            variant='h6'
-                            noWrap
-                            component='a'
-                            href='/'
-                            sx={{
-                                mr: 2,
-                                display: { xs: 'none', md: 'flex' },
-                                fontFamily: 'monospace',
-                                fontWeight: 700,
-                                letterSpacing: '.3rem',
-                                color: 'inherit',
-                                textDecoration: 'none',
-                            }}
-                        >
-                            Maru y Boris
-                        </Typography> */}
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, color: 'inherit' }}>
-                        <IconButton
-                            size='large'
-                            aria-label='account of current user'
-                            aria-controls='menu-appbar'
-                            aria-haspopup='true'
-                            onClick={handleOpenNavMenu}
-                            color='inherit'
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id='menu-appbar'
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: 'block', md: 'none' },
-                            }}
-                        >
-                            {pages.map(page => (
-                                <MenuItem color='primary' key={page} onClick={handleCloseNavMenu}>
-                                    <Typography color='primary' textAlign='center'>
-                                        {page}
-                                    </Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
+                    <Box p={1}>
+                        <Box component='img' src={maruYBoris} width={100} />
                     </Box>
 
-                    {/* <Typography
-                            variant='h5'
-                            noWrap
-                            component='a'
-                            href=''
-                            sx={{
-                                mr: 2,
-                                display: { xs: 'flex', md: 'none' },
-                                flexGrow: 1,
-                                fontFamily: 'monospace',
-                                fontWeight: 700,
-                                letterSpacing: '.3rem',
-                                color: 'inherit',
-                                textDecoration: 'none',
-                            }}
-                        >
-                            Maru y Boris
-                        </Typography> */}
-
+                    {/*
                     <Stack
                         direction='row'
                         justifyContent='right'
                         alignItems='center'
                         sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
                     >
-                        {/* pages.map(page => (
+                         pages.map(page => (
                             <Button key={page} onClick={handleCloseNavMenu} sx={{ my: 2, display: 'block', color: 'inherit' }}>
                                 {page}
                             </Button>
-                        )) */}
-                    </Stack>
+                        )) 
+                    </Stack>*/}
                 </Toolbar>
             </Container>
         </AppBar>
