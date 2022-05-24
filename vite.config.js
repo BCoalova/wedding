754b1a019fa02a7b576dbae24c53a8878e3a75b0
bcoalova <coalovaboris@gmail.com>
2022-05-24 08:46:15 -0300

    test solution?

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    build: {
-        emptyOutDir: false,
-    },
-    rollupOptions: {
-        input: {
-            app: 'CNAME',
-        },
-        output: {
-            entryFileNames: 'CNAME',
-            dir: './',
-        },
-    },
 })
